8c234907a13a6b4a76abf8f852e0ecde0d28b58a
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-10-23 21:11:36 -0300

    OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,76 @@
-import { useEffect, useState } from "react";
-
+import { useState } from "react";
 import "./styles.css";
 
-function JobPosting({ url, by, time, title }) {
-  return (
-    <div className="post" role="listitem">
-      <h2 className="post__title">
-        {url ? (
-          <a
-            className="post__title__link"
-            href={url}
-            target="_blank"
-            rel="noopener"
-          >
-            {title}
-          </a>
-        ) : (
-          title
-        )}
-      </h2>
-      <p className="post__metadata">
-        By {by} &middot; {new Date(time * 1000).toLocaleString()}
-      </p>
-    </div>
-  );
+const NUMBER_OF_SIDES = 6;
+
+const DIE_FACE_POSITIONS = [
+  ["center"],
+  ["top-right", "bottom-left"],
+  ["top-right", "center", "bottom-left"],
+  ["top-left", "top-right", "bottom-left", "bottom-right"],
+  ["top-left", "top-right", "center", "bottom-left", "bottom-right"],
+  [
+    "top-left",
+    "top-right",
+    "center-left",
+    "center-right",
+    "bottom-left",
+    "bottom-right",
+  ],
+];
+
+const generateId = (() => {
+  let id = 0;
+  return function generateId() {
+    return ++id;
+  };
+})();
+
+function generateDices(numberOfDice) {
+  return Array.from({ length: numberOfDice }, () => ({
+    value: Math.ceil(Math.random() * NUMBER_OF_SIDES),
+    id: generateId(),
+  }));
 }
 
-const PAGE_SIZE = 6;
-
-export default function App() {
-  const [fetchingJobDetails, setFetchingJobDetails] = useState(false);
-  const [jobIds, setJobIds] = useState(null);
-  const [jobs, setJobs] = useState([]);
-  const [page, setPage] = useState(0);
-
-  useEffect(() => {
-    console.log("fetching jobs for page", page);
-    fetchJobs(page);
-  }, [page]);
-
-  async function fetchJobIds(currPage) {
-    let jobs = jobIds;
-    if (!jobs) {
-      const res = await fetch(
-        "https://hacker-news.firebaseio.com/v0/jobstories.json"
-      );
-      jobs = await res.json();
-      setJobIds(jobs);
-    }
-
-    const start = currPage * PAGE_SIZE;
-    const end = start + PAGE_SIZE;
-    return jobs.slice(start, end);
-  }
-
-  async function fetchJobs(currPage) {
-    const jobIdsForPage = await fetchJobIds(currPage);
-
-    setFetchingJobDetails(true);
-    const jobsForPage = await Promise.all(
-      jobIdsForPage.map((jobId) =>
-        fetch(`https://hacker-news.firebaseio.com/v0/item/${jobId}.json`).then(
-          (res) => res.json()
-        )
-      )
-    );
-    setJobs([...jobs, ...jobsForPage]);
-
-    setFetchingJobDetails(false);
-  }
+const App = () => {
+  const [dices, setDiceValues] = useState([]);
 
   return (
-    <div className="app">
-      <h1 className="title">Hacker News Jobs Board</h1>
-      {jobIds == null ? (
-        <p className="loading">Loading...</p>
-      ) : (
-        <div>
-          <div className="jobs" role="list">
-            {jobs.map((job) => (
-              <JobPosting key={job.id} {...job} />
-            ))}
-          </div>
-          {jobs.length > 0 && page * PAGE_SIZE + PAGE_SIZE < jobIds.length && (
-            <button
-              className="load-more-button"
-              disabled={fetchingJobDetails}
-              onClick={() => setPage(page + 1)}
-            >
-              {fetchingJobDetails ? "Loading..." : "Load more jobs"}
-            </button>
-          )}
-        </div>
-      )}
+    <div className="wrapper">
+      <form
+        className="dice-form"
+        onSubmit={(event) => {
+          // To prevent a page reload.
+          event.preventDefault();
+
+          const data = new FormData(event.target);
+          const numberOfDice = data.get("dice");
+          setDiceValues(generateDices(+numberOfDice));
+        }}
+      >
+        <label htmlFor="dice">Number of dice</label>
+        <input id="dice" name="dice" type="number" min="1" max="99" />
+
+        <button type="submit">Roll</button>
+      </form>
+
+      <div className="dices">
+        {dices.map(({ id, value }) => {
+          const dotPositions = DIE_FACE_POSITIONS[value - 1];
+          return (
+            <div key={id} className="dice">
+              <div className="dots">
+                {dotPositions.map((dotPosition) => (
+                  <div key={dotPosition} className={`dot ${dotPosition}`} />
+                ))}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
+
+export default App;
